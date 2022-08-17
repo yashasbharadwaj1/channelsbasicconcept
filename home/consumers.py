bed485a320f406324fc5711a4741c5819fd05bca
yashasbharadwaj1 <yashasbharadwaj6@gmail.com>
2022-08-17 14:02:24 +0530

    finished first proj abhijeet

---
+++ home/consumers.py
@@ -0,0 +1,27 @@
+from channels.generic.websocket import WebsocketConsumer
+from asgiref.sync import async_to_sync
+import json
+
+
+class TestConsumer(WebsocketConsumer):
+    def connect(self):
+        self.room_name = "test_consumer"
+        self.room_group_name = "test_consumer_group"
+        async_to_sync(self.channel_layer.group_add)(
+             self.room_group_name,
+             self.channel_name
+        )
+        self.accept()
+        self.send(text_data=json.dumps({'status': 'django-connection succesfull'}))
+
+    def receive(self, text_data):
+        print(text_data)
+
+    def disconnect(self, *args, **kwargs):
+        pass
+
+    def send_notification(self,event):
+        print('send notification')
+        data = json.loads(event.get('value'))
+        self.send(text_data=json.dumps({'payload':data}))
+
---
+++ home/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.contrib.auth.models import User
+from channels.layers import get_channel_layer
+from asgiref.sync import async_to_sync
+import json
+
+
+# Create your models here.
+class Notification(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    notification = models.TextField(max_length=100)
+    is_seen = models.BooleanField(default=False)
+
+    def save(self, *args, **kwargs):
+        channel_layer = get_channel_layer()
+        notification_objs = Notification.objects.filter(is_seen=False).count()
+
+        data = {'count': notification_objs, 'current_notification': self.notification}
+
+        async_to_sync(channel_layer.group_send)(
+            'test_consumer_group', {
+                'type': 'send_notification',
+                'value': json.dumps(data)
+            }
+        )
+        super(Notification, self).save(*args, **kwargs)
